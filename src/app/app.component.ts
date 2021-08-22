d69b123d4c85ff145319e0530931b536c405385a
Noah Cordova <mrcordova@ucdavis.edu>
2021-08-22 13:29:59 -0700

    created Form object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+
+
+  constructor(shippingAddressForm: FormGroup) {
+    shippingAddressForm = new FormGroup({
+      'first': new FormControl(null, Validators.required),
+      'last': new FormControl(null),
+      'company': new FormControl(null),
+      'address': new FormControl(null),
+      'apartment': new FormControl(null),
+      'city': new FormControl(null),
+      'state': new FormControl(null),
+      'postal-code': new FormControl(null),
+    });
+  }
+
   title = 'joshs-frogs-interview';
 }
