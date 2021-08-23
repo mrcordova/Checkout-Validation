40058bad5bb640c66a45e1593e9bb7608d10df35
Noah Cordova <mrcordova@ucdavis.edu>
2021-08-23 14:53:18 -0700

    states array 4 confirmation of state's existence

--- src/app/StateValidator.ts
+++ src/app/StateValidator.ts
@@ -1,7 +1,62 @@
 import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
 
+
+const usStates = [
+    "AL",
+	"AK",
+	"AZ",
+	"AR",
+	"CA",
+	"CO",
+	"CT",
+	"DE",
+	"DC",
+	"FL",
+	"GA",
+	"HI",
+	"ID",
+	"IL",
+	"IN",
+	"IA",
+	"KS",
+	"KY",
+	"LA",
+	"ME",
+	"MD",
+	"MA",
+	"MI",
+	"MN",
+	"MS",
+	"MO",
+	"MT",
+	"NE",
+	"NV",
+	"NH", 
+	"NJ",
+	"NM", 
+	"NY" ,
+	"NC",
+	"ND" ,
+	"OH",
+	"OK",
+	"OR",
+	"PA", 
+	"RI", 
+	"SC", 
+	"SD", 
+	"TN", 
+	"TX", 
+	"UT", 
+	"VT", 
+	"VA", 
+	"WA",
+	"WV",  
+	"WI", 
+	"WY"
+]
+
 export function StateValidator(): ValidatorFn {
-    return (control:AbstractControl) : ValidationErrors | null => {
+    return (control:AbstractControl) : ValidationErrors | null = {
         if (!control.value) return null;
 
         const value = control.value;
@@ -9,6 +64,6 @@ export function StateValidator(): ValidatorFn {
         
 
 
-        return notNumber ? {notNumber} : null;
+        return {}
     }
 }
